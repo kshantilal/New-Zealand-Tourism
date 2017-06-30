ef7f72579b6d12330b7b5bfd3120ec15ce4a997b
Kunal Shantilal <845870587@ken20-202-08.local>
2017-06-30 14:13:00 +1200

    added in validation for max number

--- js/script.js
+++ js/script.js
@@ -1,30 +1,14 @@
 $(document).ready(function(){
 
-// var	numberofPeople = false;
-// Validate Number of people
-
-// $("#People").keyup(function(){
-// 	$(this).parent().find('span.errors');
-// 	var peopleErrors = $(this).parent().find('span.input-errors')
-// 	peopleErrors.empty();
-// 	if ($(this).val().length === 0) {
-// 		peopleErrors.text("This field is required").addClass("error");
-// 		return;
-// 	}
-// 	numberofPeople = true
-
-
-
-// });
 
 $("#People").focus(function(){
 	if ($(this).val().length === 0) {
 		$(this).parent().find('span.input-errors').empty();
 		$(this).parent().find('span.input-errors').append("<ul class='error'></ul>");
 		$(this).parent().find('span.input-errors ul').append(
-								"<li class='required'>This is required</li>"+
 								"<li class='numbers'>Must include 1 number</li>"
-
+								
+								
 							)
 
 	}
@@ -32,15 +16,7 @@ $("#People").focus(function(){
 
 
 }).keyup(function(){
-	console.log($(this).val());
-	// if ($(this).val().length !== 0) {
-	// 	$(this).parent(find).find('span-input-errors .required').remove()
-
-	// }else if ( ($(this).val().length === 0) && ($("li.required").length === 0) ) {
-	// 	$(this).parent().find('span.input-errors ul').append("<li class='required'>This is required</li>")
-
-	// }
-	//Must include at least 3 numbers
+	// console.log($(this).val());
 	var	Numbers = /(?=(.*[0-9]){1}).{1,}/;
 	if ($(this).val().match(Numbers)) {
 		$(this).parent().find('span.input-errors .numbers').remove();
@@ -49,11 +25,26 @@ $("#People").focus(function(){
 		$(this).parent().find('span.input-errors ul').append("<li class='numbers'>Must include 1 number</li>")
 	}
 
+	var maxNumber = $(this).parent().find('span.input-errors')
+	if ($(this).val().length <= 2){
+		$(this).parent().find('span.input-errors .max').remove();
+	}else if ( (!$(this).val().match(maxNumber)) && ($("li.max").length === 0) ){
+		$(this).parent().find('span.input-errors ul').append("<li class='max'>Must be less than 2 numbers</li>")
+	}
 
-	});
+	// var betweenNumber = $(this).parent().find('span.input-errors')
+	if ($(this).val() > 7){
+		console.log('greater than 7');
+	} else ($(this).val() <= 7)
+		console.log('less than 7')
 
-	function required(Numbers){
+
+	function required(Numbers, maxNumber){
 		console.log('here');
 	}
+
+
+
+});
 
 });
