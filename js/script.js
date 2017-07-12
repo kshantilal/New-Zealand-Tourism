fce085667efcdacf2563bbe01f84e5116c57d236
Kunal Shantilal <KunalShantilal@Kunals-MacBook-Pro.local>
2017-07-12 14:10:48 +1200

    edited scroll on input

--- js/script.js
+++ js/script.js
@@ -227,6 +227,9 @@ $('form').on('focus', 'input[type=number]', function (e) {
 		e.preventDefault()
 	})
 });
+$('form').on('blur', 'input[type=number]', function (e) {
+	$(this).off('mousewheel.disableScroll')
+});
 
 // Google Maps
 function AutocompleteDirectionsHandler(map) {
